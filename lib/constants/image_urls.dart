b38373101dadadfda7f4041fe8a95dd7d564ee77
LeThiNgocHuyenm <ngochuyenn0502@gmail.com>
2024-01-17 15:42:16 +0700

    feat: zapp-009

--- lib/constants/image_urls.dart
+++ lib/constants/image_urls.dart
@@ -7,4 +7,12 @@ class ImageUrls {
   static const bgGradient = 'assets/images/bg-gradient.jpg';
   static const avatarDefault =
       'https://firebasestorage.googleapis.com/v0/b/messenger-b2117.appspot.com/o/images%2Favatar-default.jpg?alt=media&token=2d59e855-33f5-4fbc-81d7-00916d0450b1';
+  static const story_1 = 'assets/images/stories_1.jpg';
+  static const story_2 = 'assets/images/stories_2.jpg';
+  static const story_3 = 'assets/images/stories_3.jpg';
+  static const story_4 = 'assets/images/stories_4.jpg';
+  static const story_5 = 'assets/images/stories_5.jpg';
+  static const story_6 = 'assets/images/stories_6.jpg';
+  static const story_7 = 'assets/images/stories_7.jpg';
+  static const story_8 = 'assets/images/stories_8.jpg';
 }
--- lib/screens/story_screen.dart
+++ lib/screens/story_screen.dart
@@ -14,6 +14,17 @@ class StoryScreen extends StatefulWidget {
 }
 
 class _StoryScreenState extends State<StoryScreen> {
+  late TextEditingController _searchContro;
+  List<Map<String, String>> story = [
+    {'image': ImageUrls.story_1, 'title': 'Ngọc Huyền'},
+    {'image': ImageUrls.story_2, 'title': 'Đoàn Đại Hưng'},
+    {'image': ImageUrls.story_3, 'title': 'Phạm Thủy'},
+    {'image': ImageUrls.story_4, 'title': 'Tiên Trần'},
+    {'image': ImageUrls.story_5, 'title': 'Thành Luân'},
+    {'image': ImageUrls.story_6, 'title': 'Hoàng Phúc'},
+    {'image': ImageUrls.story_7, 'title': 'Hải Nam'},
+    {'image': ImageUrls.story_8, 'title': 'Kim Xuyến'},
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,15 +39,55 @@ class _StoryScreenState extends State<StoryScreen> {
         title: 'Story',
       ),
       body: Container(
-        padding: const EdgeInsets.all(32),
         alignment: Alignment.center,
         child: Column(
-          mainAxisSize: MainAxisSize.min,
           children: [
-            Text(
-              'Story Screen',
-              style: TypographyTheme.heading2(),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Active (184)',
+                      style: TypographyTheme.heading5(color: ColorsTheme.blackGray),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Expanded(
+                    flex: 1,
+                    child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Stories (21)',
+                          style: TypographyTheme.heading4(color: ColorsTheme.black),
+                          textAlign: TextAlign.center,
+                        ))),
+              ],
             ),
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: story.map((item) {
+                  return Card(
+                    elevation: 0,
+                    child: ListTile(
+                      onTap: () {},
+                      leading: Stack(clipBehavior: Clip.none, children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image(
+                            image: AssetImage(item['image']!),
+                            width: 200,
+                            height: 200,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ]),
+                    ),
+                  );
+                }).toList()),
           ],
         ),
       ),
